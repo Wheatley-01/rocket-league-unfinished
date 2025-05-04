2f4801cd222010e377cd572b6d847a34c132857c
Wheatley <robloxiansexclusive@gmail.com>
2025-05-04 08:22:05 +0530

    finish

---
+++ Packages/jecs.lua
@@ -0,0 +1,11 @@
+local REQUIRED_MODULE = require(script.Parent._Index["ukendio_jecs@0.5.5"]["jecs"])
+export type Archetype = REQUIRED_MODULE.Archetype 
+export type Record = REQUIRED_MODULE.Record 
+export type Entity<T > = REQUIRED_MODULE.Entity<T >
+export type Id<T > =
+REQUIRED_MODULE.Id<T >
+export type Pair<P, O> = REQUIRED_MODULE.Pair<P, O>
+export type Query<T...> = REQUIRED_MODULE.Query<T...>
+export type Observer = REQUIRED_MODULE.Observer 
+export type World = REQUIRED_MODULE.World 
+return REQUIRED_MODULE
